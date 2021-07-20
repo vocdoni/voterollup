59419593ba88b7c8d13f04a2f3cbd5494dfb1984
adria0 <nowhere@>
2021-07-20 13:22:02 +0200

    pre-challange wip

--- backend/q_n_d/server.js
+++ backend/q_n_d/server.js
@@ -14,20 +14,35 @@ class logger {
    debug(info) { console.log(info); } 
 }
 
+const { ERC20Prover } = require('@vocdoni/storage-proofs-eth');
+const rollupWasmFile = "../../contract/circuits/release/rollup.wasm";	
+const rollupZkeyFile = "../../contract/circuits/release/rollup.zkey";
+const smtKeyExistsWasmFile = "../../contract/circuits/release/smtkeyexists.wasm";	
+const smtKeyExistsZkeyFile = "../../contract/circuits/release/smtkeyexists.zkey";
 
-const circuitFile = "../../contract/circuits/circuit.circom";
-const wasmFile = "../../contract/circuits/release/circuit.wasm";	
-const zkeyFile = "../../contract/circuits/release/circuit_final.zkey";
+
+const erc20abi = `[{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"who","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"from","type":"address"},{"internalType":"address","name":"to","type":"address"},{"internalType":"uint256","name":"value","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"}]`;
+
 const VoteRollup = require("../../contract/artifacts/contracts/VoteRollup.sol/VoteRollup.json");
-const provider = new ethers.providers.InfuraProvider("rinkeby", "e43c5d110ec1432fa9071b65e9117a62");
+const provider = new ethers.providers.JsonRpcProvider("https://rinkeby.vocdoni.net");
+const web3Url = "https://rinkeby.vocdoni.net";
 const wallet = new ethers.Wallet("0xb2bb17da3946e4267cd191dc4b601fcec9143b0902a826da589ae7bc6e4a976d", provider)    
+const wallet2 = new ethers.Wallet("0xb2bb17da3946e4267cd191dc4b601fcec9143b0902a826da589ae7bc6e4a976e", provider)    
+const wallet3 = new ethers.Wallet("0xb2bb17da3946e4267cd191dc4b601fcec9143b0902a826da589ae7bc6e4a976f", provider)    
 const factory = ethers.ContractFactory.fromSolidity(VoteRollup,wallet);
-//let rollupContract = factory.attach("0xeC4e0Ce60A7D206bDf5526a63eFD0e468628dA67");
+const factory2 = ethers.ContractFactory.fromSolidity(VoteRollup,wallet2);
+const factory3 = ethers.ContractFactory.fromSolidity(VoteRollup,wallet);
 let rollupContract; 
+
+const tokenAddress =    "0x81A99588B326679E2fbc2768F81622B62ed70033";
+const balancePositionIdx = 0;
+const blockNumber = 8959397;
+const erc20Contract = new ethers.Contract(tokenAddress, erc20abi, wallet);
 
 let roll = new rollup.Rollup(4,10);
 
 let vote1 = {
+  address: wallet.address,
   votePbkAx: 1891156797631087029347893674931101305929404954783323547727418062433377377293n,
   votePbkAy: 14780632341277755899330141855966417738975199657954509255716508264496764475094n,
   voteSigS: 2579303388791659297061741374259677256055471270157957617441871160947938236302n,
@@ -37,6 +52,7 @@ let vote1 = {
 };
 
 let vote2 = {
+  address: wallet2.address,
   votePbkAx: 16854128582118251237945641311188171779416930415987436835484678881513179891664n,
   votePbkAy: 8120635095982066718009530894702312232514551832114947239433677844673807664026n,
   voteSigS: 1289667425188663527301547897148053480741735311212388730155024428617701775067n,
@@ -46,6 +62,7 @@ let vote2 = {
 };
 
 let vote3 = {  
+  address: wallet3.address,
   votePbkAx: 17184842423611758403179882610130949267222244268337186431253958700190046948852n,
   votePbkAy: 14002865450927633564331372044902774664732662568242033105218094241542484073498n,
   voteSigS: 2049813369450170712099093026932660045613544503110336874696805294622390016342n,
@@ -54,47 +71,123 @@ let vote3 = {
   voteValue: 30000n
 };
 
-    function b256(n) {
-       let nstr = n.toString(16);
-       while (nstr.length < 64) nstr = "0"+nstr;
-       nstr = `0x${nstr}`;
-       return nstr;
-    }
-
-
+function b256(n) {
+   let nstr = n.toString(16);
+   while (nstr.length < 64) nstr = "0"+nstr;
+   nstr = `0x${nstr}`;
+   return nstr;
+}
 
 async function do_deploy() {
     console.log("deploying contract...");
-    rollupContract = await factory.deploy();
-    console.log("deploy tx  ",rollupContract.deployTransaction.hash);
+    let tokenAddress = "0x0ebe88c216a7eaff0ae46ce01fd685303c4797bd";
+    let tokenSlot = 0;
+    let tokenBlockNumber = 8942234;
+    rollupContract = await factory.deploy(
+	    tokenAddress,
+	    tokenSlot,
+	    tokenBlockNumber
+    );
+    console.log("deploy tx ",rollupContract.deployTransaction.hash);
     await rollupContract.deployTransaction.wait()
     console.log("deployed");
 }
 
 async function do_rollup(votes) {
     let input = await roll.rollup(votes);
-    let proof = await snarkjs.groth16.fullProve(input,wasmFile,zkeyFile /*, new logger()*/ );
+    let proof = await snarkjs.groth16.fullProve(input,rollupWasmFile,rollupZkeyFile /*, new logger()*/ );
     let callData = await snarkjs.groth16.exportSolidityCallData(proof.proof, proof.publicSignals);
+    
+    let addrs = Array.from(votes, v => v.address );
+    while (addrs.length < roll.batchSize) addrs.push("0x0000000000000000000000000000000000000000"); 
+   
+    console.log(addrs);
 
     console.log("sending rollup tx...");
+      
     let tx = await rollupContract.collect(
 	b256(input.newNullifiersRoot),
 	input.result,
 	input.nVotes,
-	input.votePbkAx.map(v=>b256(v)),
+	addrs,
 	[ proof.proof.pi_a[0], proof.proof.pi_a[1] ],
 	[ [proof.proof.pi_b[0][1],proof.proof.pi_b[0][0]],[proof.proof.pi_b[1][1],proof.proof.pi_b[1][0]] ],
 	[ proof.proof.pi_c[0], proof.proof.pi_c[1] ],
     );
+    
     let res = await tx.wait();
     console.log("rollup tx mined");
 }
 
+async function getStorageProof(holderAddress) {
+	const balanceSlot = ERC20Prover.getHolderBalanceSlot(holderAddress, balancePositionIdx)
+	const storageProover = new ERC20Prover(web3Url)
+	const data = await storageProover.getProof(tokenAddress, [balanceSlot], blockNumber, true)
+
+	return data;
+	const { proof, block, blockHeaderRLP, accountProofRLP, storageProofsRLP } = data
+        console.log(data);	
+}
+
+async function do_challange(bbjPbk, ethAddress) {
+    const storageProof = getStorageProof(ethAddress);
+    const root = rollupContract.nullifiersRoot();
+
+    let input = await roll.smtKeyExists(bbjPbk);
+    let proof = await snarkjs.groth16.fullProve(input,smtKeyExistsWasmFile,smtKeyExistsZkeyFile/*, new logger()*/ );
+    let callData = await snarkjs.groth16.exportSolidityCallData(proof.proof, proof.publicSignals);
+
+    console.log("sending rollup tx...");
+    let tx = await rollupContract.challange(
+	ethAddress,
+	storageProof.blockHeaderRLP,
+	storageProof.accountProofRLP,
+	storageProof.storageProofsRLP[0],
+	[ proof.proof.pi_a[0], proof.proof.pi_a[1] ],
+	[ [proof.proof.pi_b[0][1],proof.proof.pi_b[0][0]],[proof.proof.pi_b[1][1],proof.proof.pi_b[1][0]] ],
+	[ proof.proof.pi_c[0], proof.proof.pi_c[1] ],
+    );
+
+    let res = await tx.wait();
+    console.log("challange tx mined");
+}
+/*
+async function checker() {	
+	rollupContract.on("Voted", (voters) => {
+		// check that voters are ok
+		for (let n=0;n<voters.length;n++) {
+			// check if contains tokens
+			const ethAddress = await rollupContract.keys(voters[n]);
+			let challange = false;
+			if (ethAddress=="0x0000000000000000000000000000000000000000") {
+				challange = true;
+			} else {
+				if (await erc20token.balanceOf(ethAddress) == 0) {
+					challange = true;
+				}
+			}
+			if (challange) {
+			
+			}
+		}
+	})
+}
+*/
 async function start() {
+	
+	//await getStorageProof(wallet.address);
 	if (rollupContract === undefined ) {
 		await do_deploy();
 	}
-	await (await rollupContract.registerVoter(b256(vote1.votePbkAx))).wait();
+	const c2 = new ethers.Contract(rollupContract.address,VoteRollup.abi, wallet2);
+	const c3 = new ethers.Contract(rollupContract.address,VoteRollup.abi, wallet3);
+	const r1 = await rollupContract.registerVoter(b256(vote1.votePbkAx));
+	const r2 = await c2.registerVoter(b256(vote2.votePbkAx));
+	const r3 = await c3.registerVoter(b256(vote3.votePbkAx));
+        await r1.wait();
+	await r2.wait();
+	await r3.wait();
+
 	await do_rollup([vote1,vote2]);
 	await do_rollup([vote3]);
 	console.log("result=",await rollupContract.result());
@@ -127,5 +220,3 @@ async function start() {
         console.log(e);
     }
 })();
-
-
--- contract/contracts/VoteRollup.sol
+++ contract/contracts/VoteRollup.sol
@@ -1,74 +1,99 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.6.0;
+pragma experimental ABIEncoderV2;
 
-import { Verifier } from "../circuits/release/verifier.sol";
+import { Verifier as RollupVerifier } from "../circuits/release/rollup.sol";
+import { Verifier as SMTKeyExistsVerifier } from "../circuits/release/smtkeyexists.sol";
 import { StorageProof } from "./lib.sol";
 
-contract VoteRollup is Verifier {
+contract VoteRollup {
   bytes32 public nullifierRoot;
   uint256 public result;   
   uint256 public count;
+  address tokenAddress;
+  uint256 balanceMappingPosition;
+  uint256 blockNumber;
 
   // Modulus zkSNARK
   uint256 constant _RFIELD = 21888242871839275222246405745257275088548364400416034343698204186575808495617;
 
+  event Voted(address[]);  
   event Challanged();
+  
   StorageProof storageProof;
+  RollupVerifier rollupVerifier;
+  SMTKeyExistsVerifier smtKeyExistsVerifier;
+  
+  mapping(address=>bytes32) public keys;
 
-  mapping(address=>bytes32) keys;
-
-  constructor() public {
+  constructor(address _tokenAddress, uint256 _balanceMappingPosition, uint256 _blockNumber) public {
      storageProof = new StorageProof();
+     rollupVerifier = new RollupVerifier();
+     smtKeyExistsVerifier = new SMTKeyExistsVerifier();
+     tokenAddress = _tokenAddress;
+     balanceMappingPosition = _balanceMappingPosition;
+     blockNumber = _blockNumber;
   }
 
   function registerVoter(bytes32 bbjPbkX) external {
   	keys[msg.sender] = bbjPbkX;
   }
-
-  function collect(bytes32 _nullifierRoot, uint256 _result, uint256 _count, bytes32[] calldata _voters,
+  
+  function collect(bytes32 _nullifierRoot, uint256 _result, uint256 _count, address[] calldata _voters,
 		   uint[2] calldata _proofA, uint[2][2] calldata _proofB, uint[2] calldata _proofC) external {
-      
+     
       // verify proof, allow bypass for tests
       if (_proofA[0] != 0) {
-          bytes memory packed = abi.encodePacked(nullifierRoot,_nullifierRoot,_result,_count,_voters);
-          uint[1] memory inputValues = [ uint256(sha256(packed)) % _RFIELD ];
-          require(verifyProof(_proofA, _proofB, _proofC, inputValues), "invalid-proof");
+	  bytes32[] memory voters = new bytes32[](_voters.length);
+	  for (uint256 n =0;n<_count; n++) { 
+		voters[n] = keys[_voters[n]];
+		require(voters[n]!=0, "voter not in census");
+	  }
+          bytes memory packed = abi.encodePacked(nullifierRoot,_nullifierRoot,_result,_count,voters);
+	  uint[1] memory inputValues = [ uint256(sha256(packed)) % _RFIELD ];
+          require(rollupVerifier.verifyProof(_proofA, _proofB, _proofC, inputValues), "invalid-proof");
       }
 
       // accumulate result 
       result += _result;
       count += _count;
       nullifierRoot = _nullifierRoot;
+
+      emit Voted(_voters); // other alternatives is just to scan all txs, or just emit VotedInBlock(blockno) to reduce costs 
   }
 
-  function challange_not_in_census(
-  	// ERC20 proof
+  function challange(
 	address _voter,
-	address _tokenAddress,
-	uint256 _balanceMappingPosition,
-	uint256 _blockNumber,
-	bytes   calldata _blockHeaderRLP,
-	bytes   calldata _accountStateProof,
-	bytes   calldata _storageProof,
 
-	// Snark proof that bbjPbk voted 
-	uint[8] calldata _proofABC
+	bytes   memory _blockHeaderRLP,
+	bytes   memory _accountStateProof,
+	bytes   memory _storageProof,
+
+	uint[2] memory _proofA,
+	uint[2][2] memory _proofB,
+	uint[2] memory _proofC
   ) external {
-	// check that the voter do not have any token
-	uint256 balance = StorageProof.getERC20Balance(_voter, _tokenAddress, _balanceMappingPosition,
-	   _blockNumber, _blockHeaderRLP, _accountStateProof, _storageProof);
-	require(balance == 0);
-
 	// check that the voter registered the key
         bytes32 bbjPbkX = keys[_voter];
 	require(bbjPbkX != 0);
 
+	// check that the voter do not have any token
+	uint256 balance = storageProof.getERC20Balance(
+		_voter, 
+		tokenAddress, 
+		balanceMappingPosition,
+	   	blockNumber,
+		_blockHeaderRLP, 
+		_accountStateProof, 
+		_storageProof
+	);
+	
+	require(balance == 0);
+
 	// check that the voter exists in the nullifiers
-	// require(verifierProof2(bbkPbkX, nullifierRoot, _proofA, _proofB, _proofC); 
+        uint[2] memory inputValues = [ uint(nullifierRoot) , uint(bbjPbkX) ];
+	require(smtKeyExistsVerifier.verifyProof(_proofA, _proofB, _proofC, inputValues)); 
   
         emit Challanged();
   }
-*/
 }
-
-
